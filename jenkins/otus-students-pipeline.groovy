71a7b81683817eac217f3b3b57c111a031d07941
Pasha <svyatoj1988@gmail.com>
2022-06-20 21:57:19 +0300

    Add jenkins integration

---
+++ jenkins/otus-students-pipeline.groovy
@@ -0,0 +1,27 @@
+timeout(60) {
+    node('python-jenkins-slave') {
+        timestamps {
+            wrap([$class: 'BuildUser']){
+                summary = """<b>Owner:</b> ${env.BUILD_USER}"""
+            stage('Checkout') {
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [[name: 'refs/heads/master']],
+                    doGenerateSubmoduleConfigurations: false,
+                    submoduleCfg: [],
+                    userRemoteConfigs: [[credentialsId: 'jenkins', url: 'git@github.com:saint88/selenium_otus_students.git']]
+                ])
+            }
+            stage('Clean project') {
+                sh """
+                    mvn clean
+                """
+            }
+            stage('Running tests') {
+                sh """
+                mvn test -Dbase.url=${env.BASE_URL}
+                """
+            }
+        }
+    }
+}
